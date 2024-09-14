240d9d44090ca52e884f02bdf805dc6d7e52a1be
alvescee <alvescauaferreira@icloud.com>
2024-09-14 18:54:32 -0300

    Correção e aprimoramento do código sobre ratio button
    O código está mais limpo e a troca de visualização está funcionando melhor.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// About the map
+
 window.addEventListener('load', () => {
     const city = [-22.28036, -42.534943];
     const zoom = 12;
@@ -18,17 +20,68 @@ function onMapClick(e) {
     console.log(e)
 }
 
+// About the ratio button to change the view
+
+let waitBeforeWheelAgain = false;
+const durationInAnimationUp = 0.3;
+
 window.addEventListener('load', () => {
 
     const input = document.querySelectorAll("input.view");
+    const sect = document.querySelector("section");
 
-    const changeView = (input, fun) => {
-        input.addEventListener('click', () => {
-            fun();
-        })        
-    }
-    
-    changeView(input[0], () => {console.log(input[0].value)});
+    input[0].addEventListener('change', () => {
+        modeView(true, 0);
+    })
 
-    changeView(input[1], () => {console.log(input[1].value)});
+    input[1].addEventListener('change', () => {
+        modeView(false, "-100%");
+    })
+
+    sect.addEventListener("wheel", () => {
+
+        if (waitBeforeWheelAgain == true) {
+            return;
+        }
+
+        if (input[0].checked == true) {
+            input[1].checked = true;
+            modeView(false, "-100%");
+        } else {
+            input[0].checked = true;
+            modeView(true, 0);
+        }
+
+        waitBeforeWheelAgain = true;
+
+        setTimeout(() => {
+            waitBeforeWheelAgain = false;
+        }, durationInAnimationUp * 1000);
+    })
+
 })
+
+function modeView (isReverse, topFinal) {
+
+    const view = name => {
+        let e = document.querySelector('article.' + name).style;
+        
+        e.animationName = "directionView";
+        e.animationDuration = durationInAnimationUp + "s";
+
+        if (isReverse) {
+            e.animationDirection = "reverse";    
+        }
+
+        e.top = topFinal;
+
+        setTimeout(() => {
+            e.animation = "";
+        }, durationInAnimationUp * 1000)
+    }
+
+    view("control");
+    view("layer");
+}
+
+// ---
